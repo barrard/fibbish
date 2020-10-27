5d438d06e6ab36f84ffc4d3223975400e8b21895
barrard <barrard@gmail.com>
2020-10-27 00:52:44 -0700

    adding ip log

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ BigInt.prototype.toJSON = function() { return this.toString(); };
 
 app.get("/fibbish/:n", (req, res) => {
   let fibVals = [0, 1];
-
+  let {ip} = req
   let n  = parseFloat(req.params.n);
-  console.log(n)
+  console.log({n, ip})
   if(!number_test(n)){
     return res.json({ fibVals: "Please send a whole number" });
   }
